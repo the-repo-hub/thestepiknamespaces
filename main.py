ae6f0518908758982be07bda776f33eba0cbccd6
Victor <vzagulin@gmail.com>
2021-07-13 08:55:00 +0300

    stepik, 7/31

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-answer = [val for val in 'amazing' if val not in 'aeiou']
-print(answer)
+# add global a | a = 0
+# create foo (in) global | def foo()
+# add foo b | in def foo():
+#                   b = 0
+# create bar foo
+# add bar a
+
+i = int(input())
+dictionary = {}
+output_list = [''] * i
+counter = 0
+for j in range(0, i):
+    command, namespace, var = input().split()
+    if command == 'add':
+        dictionary[var] = namespace
+    elif command == 'create':
+        dictionary[namespace] = var
+    elif command == 'get':
+        output_list[counter] = dictionary.get(var)
+        counter += 1
+
+for k in range(0, counter + 1):
+    print(output_list[k])
