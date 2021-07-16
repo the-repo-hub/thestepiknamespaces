47d09f167fb89629bc2ff5f0c67a5cef2435632e
Victor <vzagulin@gmail.com>
2021-07-16 14:04:49 +0300

    stepik, 8/31

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-# add global a | a = 0
-# create foo (in) global | def foo()
-# add foo b | in def foo():
-#                   b = 0
-# create bar foo
-# add bar a
+def add(namespace, var):
+    dictionary[namespace]['variables'].add(var)
+    return 0
+
+
+def create(namespace, var):
+    dictionary[namespace] = {'parent': var, 'variables': set()}
+    return dictionary[namespace]
+
+
+def get(namespace, var):
+    if var in dictionary[namespace]['variables']:
+        return namespace
+    elif dictionary[namespace]['parent'] is not None:
+        for keys in dictionary.keys():
+            if var in dictionary[keys]['variables']:
+                return keys
+            else:
+                continue
+    return None
+
 
 i = int(input())
-dictionary = {}
-output_list = [''] * i
-counter = 0
-for j in range(0, i):
+dictionary = {'global': {'parent': None, 'variables': set()}}  # honorably steal from comments
+get_list = []
+for j in range(i):
     command, namespace, var = input().split()
     if command == 'add':
-        dictionary[var] = namespace
+        add(namespace, var)
     elif command == 'create':
-        dictionary[namespace] = var
+        create(namespace, var)
     elif command == 'get':
-        output_list[counter] = dictionary.get(var)
-        counter += 1
-
-for k in range(0, counter + 1):
-    print(output_list[k])
+        get_list.append(get(namespace, var))
+for k in get_list:
+    print(k)
